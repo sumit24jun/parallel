e39429dc37807ed160cb9670903db128c6afa2b1
Sumit AGrawal <sumit24jun@yahoo.co.in>
2018-09-16 18:39:24 +0530

    sumit added sumit2 method

--- common.java
+++ common.java
@@ -9,5 +9,9 @@ public class common {
 	public void sumit1() {
 		System.out.println("this is line added by sumit");
 	}
+	
+	public void sumit2() {
+		System.out.println("this is line added by sumit");
+	}
 
 }
